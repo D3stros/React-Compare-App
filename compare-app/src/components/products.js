82b834bf738b188414efd470fedbeb5f8c8db28b
D3stros <patrick.fallmann@hotmail.com>
2022-07-19 20:19:25 +0200

    Creating base hello world component

---
+++ compare-app/src/components/products.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            randomVar: ""
+            }
+        }
+
+    render() {
+
+        return (
+            <div>
+                <p>Hello World!</p>
+            </div>
+        )
+    }
+}
+
+export default Products;
